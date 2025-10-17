7e8527d7db1512a2547bc68046683f108d84cda4
Uday <2300033639@kluniversity.in>
2025-10-18 00:57:48 +0530

    aws project

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const API_ENDPOINTS = {
     // This is the full URL to the backend context on Tomcat
-    baseUrl: "http://ec2-18-222-201-215.us-east-2.compute.amazonaws.com:2025/airlinemanagement" 
+    baseUrl: "http://ec2-18-222-201-215.us-east-2.compute.amazonaws.com:2025" 
 };
