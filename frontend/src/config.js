628d5ea781d373635c7fb5861db6ffd2ac9798b4
Uday <2300033639@kluniversity.in>
2025-10-18 00:48:27 +0530

    modified

---
+++ frontend/src/config.js
@@ -0,0 +1,4 @@
+export const API_ENDPOINTS = {
+    // This is the full URL to the backend context on Tomcat
+    baseUrl: "http://ec2-18-222-201-215.us-east-2.compute.amazonaws.com:2030/airlinemanagement" 
+};
